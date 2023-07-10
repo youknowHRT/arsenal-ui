644af883753860b68c4ef7cf5227e760c8352b26
youknowHRT <1021300769@qq.com>
2023-07-11 07:26:10 +0800

    feat：v-drag

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,7 +2,11 @@
   <div class="sonOfApp">
     <oo-button>🏀</oo-button>
     <!-- <oo-jsx-button>🌹</oo-jsx-button> -->
-    <oo-icon color="blue" size="18px">
+    <!-- <oo-icon color="blue" size="18px" v-drag> -->
+    <oo-icon color="blue" size="18px" v-drag="{
+      fn:getMoreData,
+      ace:'ace'
+    }" style="position: fixed">
       <Male/>
     </oo-icon>
     
@@ -12,7 +16,11 @@
 <script setup lang="ts">
 import * as OoSvg from '@arsenal-ui/icons-vue'
 import {ArrowLeft,Male} from '@arsenal-ui/icons-vue'
+import vDrag from './directives/drag'
 defineOptions({name:'App'})
+const getMoreData = ()=>{
+  console.log('xxxxx')
+}
 </script>
 
 <style lang="scss">
@@ -20,6 +28,7 @@ defineOptions({name:'App'})
   height: 100vh;
   .sonOfApp {
     height: 100%;
+    // position: relative;
   }
   // .icon {
   //     width: 1em; height: 1em;
